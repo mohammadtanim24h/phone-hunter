261aa2d53ae22b4011c600fdbe2e287804ee1ae8
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-01 00:11:04 +0600

    dynamic phones added in ui

--- js/app.js
+++ js/app.js
@@ -4,5 +4,29 @@ const loadPhones = () => {
     // Fetching data
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayPhones(data.data));
+}
+
+// Display Phones
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    const twentyPhones = phones.slice(0, 20);
+    twentyPhones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('phone');
+        div.classList.add('col-lg-4', 'mx-auto', 'my-3');
+        div.innerHTML = `
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="${phone.image}" class="card-img-top" alt="phone-image">
+                <div class="card-body">
+                    <h4 class="card-title my-3">${phone.phone_name}</h4>
+                    <h5 class="my-3">${phone.brand}</h5>
+                    <button class="btn btn-primary px-4 py-2">Details</button>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(div);
+    })
 }
